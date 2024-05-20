8f1136f5a83b199fde8a96177dae4f7e5f303b4f
HP 840 G4 <basilakk7@gmail.com>
2024-05-20 11:32:35 +0530

    initial commit

--- lib/features/screen/userPage.dart
+++ lib/features/screen/userPage.dart
@@ -2,7 +2,9 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:gradients/gradients.dart';
 
+import '../../colorpage.dart';
 import '../../core/controllers/user_controller.dart';
 import '../../main.dart';
 
@@ -18,72 +20,86 @@ class _UsersPageState extends ConsumerState<UsersPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SingleChildScrollView(
-        child: Column(
-          children: [
-            ref.watch(StreamDataProvider)
-                .when(data: (data) =>  ListView.builder(
-              shrinkWrap: true,
-              itemCount:data.length,
-              physics: BouncingScrollPhysics(),
-              scrollDirection: Axis.vertical,
-              itemBuilder: (context, index) {
-                return Container(
-                  width: width*0.07,
-                  height: height*0.04,
-                  margin: EdgeInsets.all(width*0.03),
-                  decoration: BoxDecoration(
-                      color: Colors.grey,
-                      borderRadius: BorderRadius.circular(width*0.015),
-                      border: Border.all(color: Colors.black)
+        child: Padding(
+          padding: EdgeInsets.all(width*0.02),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              ref.watch(StreamDataProvider).when(data: (data) => Container(
+                width: width*0.8,
+                height: height*1,
+                child: GridView.builder(
+                  itemCount:data.length,
+                  gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                    childAspectRatio: 5,
+                    crossAxisSpacing:width*0.03,
+                    mainAxisSpacing: height*0.05,
+                    crossAxisCount: 2,
                   ),
-                  child: Padding(
-                    padding:  EdgeInsets.all(width*0.03),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                      children: [
-                        Column(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
+                  physics: BouncingScrollPhysics(),
+                  shrinkWrap: true,
+                  scrollDirection: Axis.vertical,
+                  itemBuilder: (context, index) {
+                    return Container(
+                         width: width*0.3,
+                         height: height*0.03,
+                         decoration: BoxDecoration(
+                           gradient: LinearGradientPainter(
+                             colors: <Color>[ColorPage.primarycolor, ColorPage.fifthcolor],
+                           ),
+                         boxShadow:[
+                         BoxShadow(
+                         color: ColorPage.thirdcolor.withOpacity(0.10),
+                     blurRadius: 3,
+                     spreadRadius: 2,
+                     offset:Offset(0, 4),
+                       )
+                    ] ,
+                    borderRadius: BorderRadius.circular(width*0.01),),
+                        child: Padding(
+                          padding: EdgeInsets.only(left: width*0.015),
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
                             Row(
                               children: [
                                 Text("Name : ",style: TextStyle(fontWeight: FontWeight.w600),),
                                 Text(data[index].name.toString(),style: TextStyle(fontWeight: FontWeight.w600),),
                               ],
                             ),
-                            Row(
-                              children: [
-                                Text("Email : ",style: TextStyle(fontWeight: FontWeight.w600),),
-                                Text(data[index].email.toString(),style: TextStyle(fontWeight: FontWeight.w600),),
-                              ],
-                            ),
-                            Row(
-                              children: [
-                                Text("Password : ",style: TextStyle(fontWeight: FontWeight.w600),),
-                                Text(data[index].password.toString(),style: TextStyle(fontWeight: FontWeight.w600),),
-                              ],
-                            ),
-                          ],
+                              Row(
+                                children: [
+                                  Text("Email : ",style: TextStyle(fontWeight: FontWeight.w600),),
+                                  Text(data[index].email.toString(),style: TextStyle(fontWeight: FontWeight.w600),),
+                                ],
+                              ),
+                              Row(
+                                children: [
+                                  Text("Password : ",style: TextStyle(fontWeight: FontWeight.w600),),
+                                  Text(data[index].password.toString(),style: TextStyle(fontWeight: FontWeight.w600),),
+                                ],
+                              ),
+                            ],
+                          ),
                         ),
-                        InkWell(
-                            onTap: () {
-                              // deleteDetails();
-                              FirebaseFirestore.instance.collection("users").doc(data[index].id).delete();
-                            },
-                            child: Icon(Icons.delete_outline))
-                      ],
-                    ),
-                  ),
-                );
-              },),
-              error: (error, stackTrace) {
-                return ScaffoldMessenger(
-                    child: Center(child: Text(error.toString())));
-              },
-              loading: () {
-                return Center(child: CircularProgressIndicator());
-              },)
+                      );
 
-          ],
+                  },
+
+
+                ),
+              ),
+                error: (error, stackTrace) {
+                  return ScaffoldMessenger(
+                      child: Center(child: Text(error.toString())));
+                },
+                loading: () {
+                  return Center(child: CircularProgressIndicator());
+                },)
+
+            ],
+          ),
         ),
       ),
     );
